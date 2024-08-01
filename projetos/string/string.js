a155d941e87b5f5bb826fe5cab530cbaac63cc22
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2024-07-31 22:13:48 -0300

    basico

---
+++ projetos/string/string.js
@@ -0,0 +1,3 @@
+console.log( typeof 'oi')
+console.log( typeof "eu quero saguadin")
+console.log( typeof `oooooo u gás`)
---
+++ projetos/string2/stri.js
@@ -0,0 +1,4 @@
+console.log('essa bagaça vai dar certo \n pq sim')
+console.log('meu nome é "lindrielton"')
+console.log(`cuida que 5 + 5 é ${5+5}`)
+console.log(" salada " + " de " + " fruta ")
