a74cffd31ff41222b10b7e13d3ed1fd913003bd0
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2024-08-05 11:38:29 -0300

    operadores ternarios

---
+++ projetos/10_ope-ternarios/operadores.js
@@ -0,0 +1,6 @@
+console.log(true ? 1 : 2)
+console.log (false ? 2 : 'esse numero não é falso')
+console.log(  "mateus" == "elton" ? " exato" : "invalido"  )
+console.log(  "mateus" == "mateus" ? "sim" : "não" )
+console.log(  10 == 9 ? 'exato' : 'errado' )
+
