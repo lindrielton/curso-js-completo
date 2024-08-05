3c47fc6081ab4f000e9498a5bf0fa3fbd8b67b61
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2024-08-04 22:35:15 -0300

    operadores logicos

---
+++ projetos/9_ope-logico/operadores.js
@@ -0,0 +1,8 @@
+console.log( 5 > 3 && 5==2 )
+console.log (5 > 3 || 2 < 1)
+console.log( "elton" == "elton" || 4 > 5 )
+console.log( 3 == 3 && 4===4 )
+console.log( ! (true && true) )
+console.log(4 <= 4 && 5 <= 5)
+console.log( 2 > 10 || 5 > 6)
+console.log(! false && false)
