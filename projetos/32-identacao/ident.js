6a677db0011a3afa8eb41314ca1b86f80ec59e5b
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2024-08-12 14:05:36 -0300

    identação

---
+++ projetos/32-identacao/ident.js
@@ -0,0 +1,15 @@
+let y = 2
+let c = 5
+
+if (y == 2 && c == 5){
+    let z = 2;
+    if( z < 3){
+        console.log(z)
+        for( i = 0; i <= 4; i = i + 1){
+            console.log(i)
+            if(i == 2){
+                console.log("i é = 2")
+            }
+        }
+    }
+}
