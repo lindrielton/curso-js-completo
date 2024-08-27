f9cc345c0b69ff83688a8c849c229c983076e5ae
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2024-08-26 22:42:18 -0300

    arrow function

---
+++ projetos/37-função/ex09/escopo.js
@@ -0,0 +1,17 @@
+let numero = 15;
+
+function acrecimo(a, l){
+
+    let numero = a - l;
+
+    if( numero <= 10){
+      let  numero = 'vixi maria';
+        console.log(numero);
+    }
+
+    console.log(numero);
+}
+
+console.log(numero);
+
+acrecimo(25, 15)
---
+++ projetos/37-função/ex10/arrow.js
@@ -0,0 +1,20 @@
+let frase = () =>{
+    console.log('olá mundo')
+
+}
+
+frase()
+
+
+const parOuInpar = (n) => {
+    return n % 2;
+}
+
+console.log((parOuInpar(11)))
+
+
+let soma = (a,b) => {
+    return a + b
+}
+
+console.log(soma(2,4))
