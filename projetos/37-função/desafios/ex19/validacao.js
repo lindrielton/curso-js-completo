9b01c922dc1543707f3c622175b7d963e4ee48da
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2024-10-28 20:54:49 -0300

    desafio feito

---
+++ projetos/37-função/desafios/ex19/validacao.js
@@ -0,0 +1,11 @@
+function VAlidando(idade){
+      if(idade < 18){
+        console.log('vc ainda não tem idade para fazer alto escola')
+      }else{
+        console.log(' vc está apto')
+      } 
+         
+      
+}
+
+VAlidando(18)
