177a20e904a52e14f765ed59ffd892ce8afe04ff
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2024-12-11 21:46:25 -0300

    arrays

---
+++ projetos/array-novo-assunto/38-arrays/arrays.js
@@ -0,0 +1,4 @@
+let arr = [1,2,3,4,5];
+let mistura = ['oi', 2 , true, false];
+console.log(arr)
+console.log(mistura)
---
+++ projetos/array-novo-assunto/mais-sobre-array/mais-sobre-arrays.js
@@ -0,0 +1,9 @@
+let arr = [1,2,4,5,6,7];
+let nomes = ['matheus', 'joao', 'Alexia']
+let bool = [true, false, true]
+
+
+console.log(arr[1]);
+console.log(nomes[2]);
+console.log(bool[0]);
+console.log(arr[arr.length - 1]);
