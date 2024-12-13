87ac64a0a99394afa7e356719baa55afcbace5e7
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2024-12-12 22:33:13 -0300

    obijetos

---
+++ projetos/array-novo-assunto/obijetos/obijetos.js
@@ -0,0 +1,11 @@
+let cachorro = {
+    patas: 4,
+    nome: 'bob',
+    acuado: function(){
+        console.log('o cachorro ta acuado!')
+    }
+};
+
+console.log(cachorro.patas)
+console.log(cachorro.nome)
+cachorro.acuado();
---
+++ projetos/array-novo-assunto/propiedads/propiedades.js
@@ -0,0 +1,4 @@
+let numero = [1,2,3,4]
+
+console.log(numero.length);
+//procurar mais propiedades para se aprimorar
