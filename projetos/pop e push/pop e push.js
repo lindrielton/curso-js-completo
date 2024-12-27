7a974f93df29fe6c37a442826d6ef28d8a28836d
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2024-12-26 22:41:52 -0300

    metodos

---
+++ projetos/pop e push/pop e push.js
@@ -0,0 +1,11 @@
+let nomes = ['matheus', 'lucas', 'victor']
+
+let elementoREMOVIDO = nomes.pop();
+
+console.log(elementoREMOVIDO);
+console.log(nomes);
+
+// 
+
+let elementoALTERADO = nomes.push('manoel')
+console.log(nomes)
---
+++ projetos/shift_unshift/pop e push.js
@@ -0,0 +1,11 @@
+let carros = ['corola', 'honda', 'nissan']
+
+let delet = carros.shift();
+console.log(delet);
+console.log(carros);
+
+// adicionando
+
+let adicionar = carros.unshift('wolkswagem');
+console.log(carros);
+console.log(carros[0]);
