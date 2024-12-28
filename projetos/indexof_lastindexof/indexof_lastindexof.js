23f014c3ec4fe08a58917770e7a195bb70367646
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2024-12-27 22:50:55 -0300

    metodos

---
+++ projetos/indexof_lastindexof/indexof_lastindexof.js
@@ -0,0 +1,6 @@
+let num = [5,6,2,4,9,6,2];
+
+console.log(num.indexOf(2));
+console.log(num.lastIndexOf(2));
+
+
---
+++ projetos/slice/slice.js
@@ -0,0 +1,11 @@
+let num = [1,2,3,4,5,6,7,8,9];
+
+console.log(num.slice(7,8));
+
+console.log(num.slice(4,6));
+
+console.log(num.slice(0));
+
+console.log(num.slice(0,-3));
+
+
