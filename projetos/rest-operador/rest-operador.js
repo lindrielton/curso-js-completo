21bc5e38dbc181a5b85d4c43ca92e1c7b66bfea2
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2025-01-06 23:13:22 -0300

    rest-operator

---
+++ projetos/rest-operador/rest-operador.js
@@ -0,0 +1,18 @@
+let num = 0;
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+let num4 = 4;
+
+function imprimirNumeros(...argts){
+    for(let i = 0; i < argts.length; i++){
+        console.log(argts[i])
+    }
+}
+
+
+imprimirNumeros(num,num1);
+console.log('separa')
+imprimirNumeros(num2,num3);
+console.log('separa')
+imprimirNumeros(1,2,3,4,5,6,7)
